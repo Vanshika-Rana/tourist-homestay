67a4e1b9de019864788a8328dc7c46829e00ecfc
Vanshika-Rana <ranavanshika172000@gmail.com>
2023-11-28 23:55:39 +0530

    number updated

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -25,7 +25,7 @@ const Footer: React.FC = () => {
                             <div className="mr-4">
                                 <FiPhone size={20} />
                             </div>
-                            <p className="mr-4">+91 9566076059, +91 7310554723</p>
+                            <p className="mr-4">+91 9543765554, +91 7310554723</p>
                         </div>
                         <div className="flex items-center mt-2">
                             <div>
